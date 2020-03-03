1a9fa52ad14af77c8efa5c8ba203f8308ce57842
Flora Fountain <nijal.flora@gmail.com>
2020-03-03 16:01:35 +0530

    Funcation File

--- wp-content/themes/flora/functions.php
+++ wp-content/themes/flora/functions.php
@@ -571,3 +571,16 @@ function disable_emojis_tinymce( $plugins ) {
 		return array();
 	}
 }
+
+add_filter( 'style_loader_src',  'sdt_remove_ver_css_js', 9999, 2 );
+add_filter( 'script_loader_src', 'sdt_remove_ver_css_js', 9999, 2 );
+
+function sdt_remove_ver_css_js( $src, $handle ) 
+{
+   $handles_with_version = ('style'); 
+
+    if ( strpos( $src, 'ver=' ) && ! in_array( $handle, $handles_with_version, true ) )
+        $src = remove_query_arg( 'ver', $src );
+
+    return $src;
+}
